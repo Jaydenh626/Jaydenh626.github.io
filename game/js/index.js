27a38ae43cf2aa3bc1ae6e84f2ca1614b3c2038b
Jaydenh626 <jaydenhalim@online.island.edu.hk>
2015-11-05 15:30:59 +0800

    lel

---
+++ game/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("#startbutton").click(function() {
+    $("#startbutton").hide();
+     $("#kanyemad").hide();
+     $("#chef").hide();
+     $("#kimface").hide();
+     $("#Title").hide();
+    $("#kw1").show();
+    $("#kw1").css("width", "380px");
+  $("#kw1").css("height", "380px");
+  $("#kw1").css("top", "40%");
+  $("#kw1").css("left", "40%");
+  });
+});
