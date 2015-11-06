eda8b2fff46654830d4b5201f2ecfcb5321394d2
Jaydenh626 <jaydenhalim@online.island.edu.hk>
2015-11-06 11:09:53 +0800

    gmae

--- game/js/index.js
+++ game/js/index.js
@@ -6,10 +6,15 @@ $(document).ready(function() {
      $("#chef").hide();
      $("#kimface").hide();
      $("#Title").hide();
-    $("#kw1").show();
-    $("#kw1").css("width", "380px");
-  $("#kw1").css("height", "380px");
-  $("#kw1").css("top", "40%");
-  $("#kw1").css("left", "40%");
+     $("#kw1").show();
+     $("#pistol").show();
   });
+
+  $("#pistol").click(function() {
+    $("#kw1").css("width", "380px");
+    $("#kw1").css("height", "380px");
+    $("#kw1").css("top", "40%");
+    $("#kw1").css("left", "40%");
+  });
+
 });
